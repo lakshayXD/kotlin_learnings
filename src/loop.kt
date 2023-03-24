17270c31d4bc4f48ef39e8eef589bc37035878f0
unknown <lakshay.chaudhary@instantsys.com>
2023-03-24 14:11:46 +0530

    loop and loop control tag function

--- src/loop.kt
+++ src/loop.kt
@@ -1,17 +1,32 @@
 class Loop{
     fun loopControl(stopLimit: Int) :Int {
-        var count: Int = 0;
+        var count: Int = 0
         for(i in 1..10){
             if(i == stopLimit){
                 break
             }
             count++
-            println("Index is :- $i");
+            println("Index is :- $i")
+        }
+        return count
+    }
+
+    fun loopTagControl(stopLimit: Int) :Int {
+        var count: Int = 0
+        topLoop@ for(i in 1..10){
+            for(j in 1..10){
+                count++
+                println("top loop index: $i")
+                println("inner loop index: $j")
+                if(i == stopLimit){
+                    topLoop@ break
+                }
+            }
         }
         return count
     }
 }
 
 fun main(args: Array<String>){
-    println("Total count is :- ${Loop().loopControl(5)}")
+    println("Total count is :- ${Loop().loopTagControl(5)}")
 }
