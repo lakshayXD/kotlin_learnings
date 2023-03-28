7b9981a9378b8fc9730953bdd5b9030944c15496
unknown <lakshay.chaudhary@instantsys.com>
2023-03-28 13:10:35 +0530

    Functions as expressions

--- src/function.kt
+++ src/function.kt
@@ -2,9 +2,11 @@ class Function{
     fun recArea(len: Int, width: Int) :Int {
         return len * width
     }
+
+    fun circleArea(radius : Int, circle : Boolean) :Double = if(circle){3.14 * radius * radius} else{-1.0}
 }
 
 fun main(args: Array<String>){
-    var area :Int = Function().recArea(10, 20);
+    var area :Double = Function().circleArea(10, true)
     print("rectangle area --> $area")
 }
