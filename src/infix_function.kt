8c497eb3f24b72ed4089640938829bc88ef19be4
unknown <lakshay.chaudhary@instantsys.com>
2023-03-28 18:40:12 +0530

    Infix Function

--- src/infix_function.kt
+++ src/infix_function.kt
@@ -1,7 +1,15 @@
 fun main(args: Array<String>){
-
+    var num1 : Int = 2
+    var num2 : Int = 20
+    var res : Int = num1 greaterThan num2
+    println("Greater number --> $res")
 }
 
-class InfixFunction{
+    infix fun Int.greaterThan(other: Int) :Int {
+        if(this > other){
+             return this
+        } else {
+             return other
+        }
+    }
 
-}
